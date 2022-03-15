9b8817ed7b6fafb7665bd4570c8e2c43eef9e64c
Marco97-exe <marco.cuce97@hotmail.com>
2022-03-15 11:11:38 +0100

    added wrapper in app.jsx to solve destructure exception

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,19 @@
 import { Navbar, Welcome, Footer, Services, Transactions } from "./components";
+import { PrenotationProvider } from "./context/PrenotationContext";
 
 const App = () => {
   return (
-  <div className="min-h-screen">
-    <div className="gradient-bg-welcome">
-      <Navbar />
-      <Welcome />
-    </div>
-    <Services />
-    <Transactions />
-    <Footer />
-  </div>
+    <PrenotationProvider>
+      <div className="min-h-screen">
+        <div className="gradient-bg-welcome">
+          <Navbar />
+          <Welcome />
+        </div>
+        <Services />
+        <Transactions />
+        <Footer />
+      </div>
+      </PrenotationProvider>
   );
 }
 
